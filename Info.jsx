e6e3915cc8190792287d63a78b9418c8920419b9
namratachanda <86581275+namratachanda@users.noreply.github.com>
2021-06-28 18:10:47 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Bootcamp</h1>
+      <p>
+        A Basic web development React Js Frontend Bootcamp. A 7 day bootcamp was
+        conducted on 15th June by Shourya Sinha.
+      </p>
+    </div>
+  );
+}
+export default Info;
